7f2f8535af9a57ebf6b65e04b9982bdd58d74257
Arvind Debnath <80598358+Suptohita@users.noreply.github.com>
2022-04-15 13:22:22 +0600

    .

--- c/trapezoid_area.c
+++ c/trapezoid_area.c
@@ -1,3 +1,18 @@
+/*
+ Question: 
+    A trapezoid is a quadrilateral with two parallel sides. The lengths of the two parallel sides are aa and bb and their distance is h.
+    The area of such a trapezoid = (1\2)*(a+b)*h 
+    Find the area of this trapezoid.
+
+input:
+    The input is given from Standard Input in the following format: 3, 4, 2 <-- (a=2, b=4, h=2)
+
+output:
+    7
+*/
+
+
+
 #include<stdio.h>
 
 int main(){
