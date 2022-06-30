3fa91825f5b101ccdb095f481885d3c9eb9f3088
Arvind Debnath <80598358+Suptohita@users.noreply.github.com>
2022-07-01 00:05:32 +0600

    bubble sort

--- c/13. bubbleSort.c
+++ c/13. bubbleSort.c
@@ -1,17 +1,17 @@
 #include <stdio.h>
 
 // bubble sort
-void swap(int arr[], int n)
+void sort(int arr[], int n)
 {
     for (int i = 0; i < n; i++)
     {
-        for (int j = i + 1; j < n; j++)
+        for (int j = 0; j < n; j++)
         {
-            if (arr[i] > arr[j])
+            if (arr[j + 1] < arr[j])
             {
-                int temp = arr[i];
-                arr[i] = arr[j];
-                arr[j] = temp;
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
             }
         }
     }
@@ -22,7 +22,7 @@ int main()
     int arr[] = {10, 1, 2, 5, 19};
     int n = sizeof(arr) / sizeof(arr[0]);
 
-    swap(arr, n);
+    sort(arr, n);
 
     // beacuse of array behaves like a pointer, it'll update automatically after calling swap()
     for (int i = 0; i < n; i++)
